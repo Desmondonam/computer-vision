bc0d7ac540af92a6bcf654e8c8d314945c75d2d4
Desmond Onam <desmondonam@gmail.com>
2022-05-03 22:34:24 +0300

    add files

--- segmentation.py
+++ segmentation.py
@@ -2,7 +2,7 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-img = cv2.imread(r'ImgPIA.jpg')
+img = cv2.imread(r'ISIC_0000074.jpg')
 b,g,r = cv2.split(img)
 rgb_img = cv2.merge([r,g,b])
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
